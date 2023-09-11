61578bbe2f591220d1bbcf11b93818eb0edfb828
benard Kiplangat <benardkiplangat97@gmail.com>
2023-09-11 21:10:18 +0300

    Added checks to determine if my_list has values in it before execution

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print(i)
+    """a script that prints integers
+
+    Prints one integer per line
+    Doesn't import any module or cast integers to strings
+    Assumes that the list contains only integers
+    Uses str.format() to print
+    """
+    if my_list:
+        for i in my_list:
+            print("{:d}".format(i))
