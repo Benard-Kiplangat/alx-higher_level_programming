ef73985fc3fffb0a7b116657eb4dc012c379500e
benard Kiplangat <benardkiplangat97@gmail.com>
2023-09-26 21:35:54 +0300

    Task 2: a class that raises exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class that defines a Square"""
+
+class Square:
+    """A class that defines a private size attribute for square"""
+
+    def __init__(self, size=0):
+        """the init function that initialize new Squares
+            size (int): The size to define the size of the new squares
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
