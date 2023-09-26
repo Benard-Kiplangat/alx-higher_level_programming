039314acdc153bceaf1504ae251d1ed3f8a745a8
benard Kiplangat <benardkiplangat97@gmail.com>
2023-09-26 21:30:15 +0300

    Task two: a class that defines a class square with hidden attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A class that defines the class Square"""
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size):
+        """init func to initialize new Squares
+        size: an integer to define the size of the new square
+        """
+        self.__size = size
