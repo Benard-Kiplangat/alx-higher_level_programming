145766991e0d502e0c654f220e90c65a0007a8cf
benard Kiplangat <benardkiplangat97@gmail.com>
2023-10-21 09:30:00 +0300

    Instantiating the Rectangle class from the Base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
+"""A class that inherits from the base class"""
 
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle is a Class that defines the properties of a Rectangle
+    
+    Attributes:
+        width (int): the width
+        height (int): the height
+        x (int): x coordinates
+        y (int): y coordinates
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Init function to create an instance of Rectangle
+
+        Arguments:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+            x (int, optional): x coordinate, defaults to 0
+            y (int, optional): y coordinate, defaults to 0
+            id (int, optional): the identity of rectangle, defaults to None
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
