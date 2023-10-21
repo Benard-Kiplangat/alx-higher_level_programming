c1897c06f688b049d4dd32fdfbdf1a90f815d5fd
benard Kiplangat <benardkiplangat97@gmail.com>
2023-10-21 09:14:48 +0300

    Updated the base class to increament the __nb_objects from the Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""A base clase for the Rectangle and Square objects"""
 
+
+class Base:
+    """
+    A base class for Rectangle and Square objects
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """A class constructor for the Base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
