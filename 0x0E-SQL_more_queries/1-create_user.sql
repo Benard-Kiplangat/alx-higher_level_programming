782ab8d8138e249c371678b744be05ff865d489a
benard Kiplangat <benardkiplangat97@gmail.com>
2023-11-15 20:22:23 +0300

    A script that creates a database user and sets password

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- A script that creates MySQL server user user_0d_1 if it does not exist
+-- The user should have all priviledges
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
