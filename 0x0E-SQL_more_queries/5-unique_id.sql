39ec4949930b41b685e4c984760b0d5c79ffd2dc
benard Kiplangat <benardkiplangat97@gmail.com>
2023-11-15 21:19:33 +0300

    A script that creates a database hbtn_0d_usa

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
--- A script that creates a table id_not_null with a unique default value for id
+-- A script that creates a table unique_id with a unique default value for id
 CREATE TABLE IF NOT EXISTS `unique_id` (
-	id INT UNIQUE, name VARCHAR(256) NOT NULL);
+	id INT UNIQUE DEFAULT(1), name VARCHAR(256) NOT NULL);
