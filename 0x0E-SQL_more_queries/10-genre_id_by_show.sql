9e8d5d69e9088c8682f1743a26a618314728b69e
benard Kiplangat <benardkiplangat97@gmail.com>
2023-11-18 13:26:52 +0300

    Task 10: a script that lists all shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- A script that lists all shows contained in hbtn_0d_tvshows
+-- that have atleast one genre linked
+SELECT gt.title, gi.genre_id FROM tv_shows AS gt
+INNER JOIN tv_show_genres AS gi
+ON gt.id = gi.show_id
+ORDER BY gt.title ASC, gi.genre_id ASC;
