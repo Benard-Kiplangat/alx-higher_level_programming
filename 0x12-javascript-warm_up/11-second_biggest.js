167816303dbece2e9fffabe2cc0fdc881d562fd3
benard Kiplangat <benardkiplangat97@gmail.com>
2023-12-12 23:20:28 +0300

    A script that finds the second biggest element in a list

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * A script that prints the second biggest integer in the list of arguments
+ */
+const myNum = [];
+
+for (let i = 2; process.argv[i]; i++) {
+  myNum[i - 2] = parseInt(process.argv[i]);
+}
+console.log(myNum);
+myNum.sort();
+console.log(myNum);
+myNum.reverse();
+console.log(myNum);
+if (myNum[0] === undefined) { console.log(0); }
+else if (myNum.length === 1) { console.log(0); }
+else console.log(myNum[1]);
