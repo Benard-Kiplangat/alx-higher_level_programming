4ae41fe011eac44988e8c69aac0ebb3c12412294
benard Kiplangat <benardkiplangat97@gmail.com>
2023-12-12 21:15:02 +0300

    Task 7: a script that prints 'C is fun' x times, number of times provided as argument

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,4 +3,10 @@
  * A script that converts a string to int and prints it
  */
 const myNum = parseInt(process.argv[2]);
-if (isNaN(myNum)) { console.log('Not a number'); } else { console.log('My number: ' + myNum); }
+if (isNaN(myNum)) { console.log('Missing number of occurrences'); } else {
+  let i = myNum;
+  while (i > 0) {
+    console.log('C is fun');
+    i--;
+  }
+}
