c022e146e0b86cb859f06267e28bd6089da9313e
benard Kiplangat <benardkiplangat97@gmail.com>
2023-12-12 21:07:40 +0300

    Initialized files for task 7 to 13

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/*
+ * A script that converts a string to int and prints it
+ */
+const myNum = parseInt(process.argv[2]);
+if (isNaN(myNum)) { console.log('Not a number'); } else { console.log('My number: ' + myNum); }
