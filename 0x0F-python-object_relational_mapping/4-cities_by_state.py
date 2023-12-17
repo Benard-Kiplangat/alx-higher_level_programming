5d01d6d783d4917ce1e8dbc81239449303d82893
benard Kiplangat <benardkiplangat97@gmail.com>
2023-12-17 18:00:34 +0300

    Checked if the name is __main__

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+cur = conn.Cursor()
+cur.execute("SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ORDER BY cities.id ASC")
+
+query_rows = cur.fechall()
+for row in query_rows:
+    print(row)
+
+cur.close()
+conn.close()
