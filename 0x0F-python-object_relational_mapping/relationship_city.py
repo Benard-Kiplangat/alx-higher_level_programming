f23f3ad88d770f4180adb94964f70fac6b77292b
benard Kiplangat <benardkiplangat97@gmail.com>
2023-12-22 20:17:37 +0300

    Removed the excess parameters in MySQLdb.connect()

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Defines the city model """
+
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ A file that contains class definition of city and links
+    MySQL table city
+
+    attributes:
+        __tablename__: the table name of MySQL database to store cities
+        id: represents a unique int, primary key, autogenarated
+        name: a column that will represent state names, not null
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
