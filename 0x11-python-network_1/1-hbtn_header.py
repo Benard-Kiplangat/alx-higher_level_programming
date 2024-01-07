b28c4b11031e58940727f922d90c65d9c1b47141
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-01-07 22:24:33 +0300

    Fixed how I imported Request and urlopen. Fixed where I source the X request id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,9 +4,12 @@ A script that takes a URL, sends a request to the URL, and displays
 the value of the X-Request-Id variable found
 in the header of the response
 """
+
+
 if __name__ == "__main__":
-    import urllib.request as request
+    from urllib.request import Request, urlopen
     from sys import argv
-    req = request.Request(argv[1])
-    with request.urlopen(req) as r:
-        print(r.headers.get('X-Request-Id'))
+    req = Request(argv[1])
+    with urlopen(req) as r:
+        headers = r.info()
+        print(headers['X-Request-Id'])
