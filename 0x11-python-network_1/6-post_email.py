513b80fc9d1f4afe20a1e95d3c3798a9db58878a
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-01-07 22:00:40 +0300

    A script that takes a URL and email, then sends the email as parameter and dispays the response

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A script that takes a URL and an email address, then sends a POST request to the
+passed URL with the email as a parameter, then displays the body of the response
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
