289ed6e4979950dd9dbb4b67ea1372b0fb998cdc
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-03-26 09:47:08 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,12 @@
-#!/usr/bin/node
-module.exports = class Rectangle {
-
-  constructor(w, h) {
-    if (h > 0 && w > 0) {
-      this.height = h;
-      this.width = w;
-    }
-  }
-
-  print() {
-    for (let i = 0; i > h; i++;) {
-      for (let j = 0; j > w; j++;) {
-        console.log('X');
-      }
-    }
-  }
-}
+#!/usr/bin/node 
+ module.exports = class Square extends require('./5-square.js') { 
+   charPrint (c = 'X') { 
+     for (let i = 0; i < this.height; i++) { 
+       let row = ''; 
+       for (let j = 0; j < this.width; j++) { 
+         row += c; 
+       } 
+       console.log(row); 
+     } 
+   } 
+ };
